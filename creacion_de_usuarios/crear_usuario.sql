f98df18faa7c35f31f693ed810897bc798e0275d
barriosevelyn488-web <barrios.evelyn.488@gmail.com>
2026-08-14 17:29:53 -0600

    feat(seguridad): crear usuarios admin_gym y entrenador_user en la base de datos

---
+++ creacion_de_usuarios/crear_usuario.sql
@@ -0,0 +1,5 @@
+USE gimnasio_force_tech;
+
+-- Creación de usuarios en el sistema
+CREATE USER 'admin_gym'@'localhost' IDENTIFIED BY 'AdminPassword123*';
+CREATE USER 'entrenador_user'@'localhost' IDENTIFIED BY 'TrainerPass123*';
---
+++ creacion_de_usuarios/evidencias/README.md
@@ -0,0 +1,6 @@
+## ejecucion de esta carpeta
+
+# 1. crear_usuario
+![alt text](image.png)
+
+# 2. 
